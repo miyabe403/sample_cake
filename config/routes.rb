715ef218cf5d031cb0ee6cbde433d7d294cfb780
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-09-17 15:32:15 +0900

    [Break Time] 休憩

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  #   sessions: "admin/sessions"
+  # }
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+  end
+  devise_for :customers
 end
